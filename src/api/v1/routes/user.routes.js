be9f62bcc6e0f20aabe16cd8a72278d999b7b73f
deepakkumar27J <90575935+deepakkumar27J@users.noreply.github.com>
2024-12-02 04:26:06 +0000

    middleware on user logged in routes

--- src/api/v1/routes/user.routes.js
+++ src/api/v1/routes/user.routes.js
@@ -4,12 +4,12 @@ const userController = require('../controllers/user.controller');
 const authenticateJWT = require('../middlewares/auth.middleware');
 
 router.post('/', userController.createUser);
-router.put('/:id', userController.updateUser);
+router.put('/:id', authenticateJWT, userController.updateUser);
 router.get('/:id', authenticateJWT, userController.getUserById);
-router.delete('/:id', userController.deleteUser);
-router.get('/rota/:userId/:rangeStartDate/:rangeEndDate', userController.getUserRota);
-router.post('/holiday',userController.addHoliday);
-router.get('/holiday/:id',userController.getHoliday);
+router.delete('/:id',authenticateJWT, userController.deleteUser);
+router.get('/rota/:userId/:rangeStartDate/:rangeEndDate',authenticateJWT, userController.getUserRota);
+router.post('/holiday', authenticateJWT, userController.addHoliday);
+router.get('/holiday/:id', authenticateJWT, userController.getHoliday);
 router.post('/sendVerificationEmail', userController.sendVerificationEmail);
 router.post('/verifyEmail', userController.verifyEmail);
 router.post('/forgotPassword', userController.forgotPassword);
